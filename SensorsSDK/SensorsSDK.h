0b1ff4e5c99628356ea9e67f07d1315fd8b41970
杨俊艺 <yjy5@meitu.com>
2020-12-01 22:40:11 +0800

    Initial Commit

---
+++ SensorsSDK/SensorsSDK.h
@@ -0,0 +1,18 @@
+//
+//  SensorsSDK.h
+//  SensorsSDK
+//
+//  Created by 杨俊艺 on 2020/12/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SensorsSDK.
+FOUNDATION_EXPORT double SensorsSDKVersionNumber;
+
+//! Project version string for SensorsSDK.
+FOUNDATION_EXPORT const unsigned char SensorsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SensorsSDK/PublicHeader.h>
+
+
